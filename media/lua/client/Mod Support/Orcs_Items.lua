b16c7ee0e10ea9cf16590dbd4f320edec894df4d
Jon Johnson <ncrawler@att.net>
2022-05-20 12:41:54 -0500

    New file and updates

---
+++ media/lua/client/Mod Support/Orcs_Items.lua
@@ -0,0 +1,27 @@
+require("ItemTweaker_Copy_CC");
+
+--Improvised Paint
+TweakItem("ImprovisedPaint.ColorYourWorldMagazine","DisplayCategory","LitR");
+TweakItem("ImprovisedPaint.DIYCraftsMagazine","DisplayCategory","LitR");
+TweakItem("ImprovisedPaint.DIYCraftsMagazine2","DisplayCategory","LitR");
+
+--Improvised Flooring
+TweakItem("ImprovisedFlooring.IFMagazineEasterEgg","DisplayCategory","LitR");
+TweakItem("ImprovisedFlooring.IFMagazineFranchise","DisplayCategory","LitR");
+TweakItem("ImprovisedFlooring.IFMagazineGarden","DisplayCategory","LitR");
+TweakItem("ImprovisedFlooring.IFMagazineIndustrial","DisplayCategory","LitR");
+TweakItem("ImprovisedFlooring.IFMagazineTiles","DisplayCategory","LitR");
+TweakItem("ImprovisedFlooring.IFMagazineUrban","DisplayCategory","LitR");
+
+--Improvised Glass
+TweakItem("ImprovisedGlass.PanWithGlassShards","DisplayCategory","Craft");
+TweakItem("ImprovisedGlass.PanWithGlassPane","DisplayCategory","Craft");
+TweakItem("ImprovisedGlass.GlassPane","DisplayCategory","Craft");
+TweakItem("ImprovisedGlass.IGMagazineGlass","DisplayCategory","LitR");
+TweakItem("ImprovisedGlass.IGMagazineWindow","DisplayCategory","LitR");
+TweakItem("ImprovisedGlass.IGMagazineCars","DisplayCategory","LitR");
+
+--Improvised Cabinetry
+TweakItem("ImprovisedCabinetry.ICMagazine1","DisplayCategory","LitR");
+TweakItem("ImprovisedCabinetry.WoodPanel","DisplayCategory","CraftCarp");
+TweakItem("ImprovisedCabinetry.SmallHinge","DisplayCategory","CraftCarp");
